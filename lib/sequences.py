17327a7ffb25412de482c64789a902daa72470a1
ArtRosh <artem.rosh17@gmail.com>
2025-07-23 15:45:24 -0700

    Set up fibonacci function to generate propriate numbers according to tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    number = []
+    if(length == 0):
+        print(number)
+    elif(length == 1):
+        number.append(0)
+        print(number)
+    elif(length == 2):
+        number.append(0)
+        number.append(1)
+        print(number)
+    elif(length >= 2):
+        number.append(0)
+        number.append(1)
+        while(length > len(number)):
+            number.append(number[-1] + number[-2])
+        print(number)
